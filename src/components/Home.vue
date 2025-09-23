092368540dbf9ac9fdb54a9161a0924149980359
Ahmad Bukhari <ahmadbukhari100703@gmail.com>
2025-09-23 23:15:50 +0800

    Update md image size Home.Vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,13 +3,13 @@
 <template>
   <main class="bg-smoke dark:bg-rich-black transition-colors duration-300 h-screen">
     <div class="flex flex-col items-center justify-center h-full text-center">
-      <img src="/src/assets/Ary.png" alt="profile" class="rounded-full border-4 border-solid border-claret dark:border-white w-48 h-48 object-cover" />
+      <img src="/src/assets/Ary.png" alt="profile" class="rounded-full border-4 border-solid border-claret dark:border-white w-38 h-38 md:w-48 md:h-48 object-cover" />
       <h1
-        class="mt-6 text-5xl font-bold py-10 font-lato bg-clip-text text-transparent [background-image:linear-gradient(to_right,#191528,#3c162f,#5c162e,#7c162e)] dark:[background-image:linear-gradient(to_right,#FFFFFF,#3C162F,#5C162E,#7C162E)]"
+        class="mt-6 text-3xl md:text-5xl font-bold py-10 font-lato bg-clip-text text-transparent [background-image:linear-gradient(to_right,#191528,#3c162f,#5c162e,#7c162e)] dark:[background-image:linear-gradient(to_right,#FFFFFF,#3C162F,#5C162E,#7C162E)]"
       >
         Data Science and Tech Enthusiast
       </h1>
-      <p class="mt-2 text-2xl max-w-xl font-lato px-4 text-gray-800 dark:text-white">I am a fresh graduate with a strong interest in machine learning, data analysis, and data visualization</p>
+      <p class="mt-2 text-lg md:text-2xl max-w-xl font-lato px-4 text-gray-800 dark:text-white">I am a fresh graduate with a strong interest in machine learning, data analysis, and data visualization</p>
     </div>
   </main>
 </template>

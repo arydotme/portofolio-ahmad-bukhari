9f6fe080c19d201440f2ed33bfd2092ea74f6812
Ahmad Bukhari <ahmadbukhari100703@gmail.com>
2025-09-23 23:17:22 +0800

    Update md text size Profile.Vue

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -19,40 +19,39 @@
         </p>
       </div>
       <!-- End About -->
-      <!-- Start Skill -->
+      <!-- Tech Stack -->
       <div class="mx-2 border-2 border-solid rounded-lg border-claret dark:border-smoke">
         <header class="font-extrabold font-lato px-6 pt-3 text-2xl text-claret">Tech Stack</header>
-        <div class="flex flex-wrap rounded-lg px-4 py-2">
+        <div class="flex flex-wrap rounded-lg px-4 py-2 text-sm md:text-lg font-lato">
           <div class="my-3 mx-2 flex flex-wrap gap-y-3 gap-x-1">
-            <span class="font-lato bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Javascript</span>
-            <span class="font-lato bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Python</span>
-            <span class="font-lato bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">NodeJS</span>
-            <span class="font-lato bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">RESTFull API</span>
-            <span class="font-lato bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Flask</span>
-
-            <span class="font-lato bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Git</span>
+            <span class="bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Javascript</span>
+            <span class="bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Python</span>
+            <span class="bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">NodeJS</span>
+            <span class="bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">RESTFull API</span>
+            <span class="bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Flask</span>
+            <span class="bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Git</span>
           </div>
         </div>
       </div>
-      <!-- End Skill -->
-      <!-- Start Skill -->
+      <!-- End Tech Stack -->
+      <!-- Start Data Tools -->
       <div class="mx-2 border-2 border-solid rounded-lg border-claret dark:border-smoke">
         <header class="font-extrabold font-lato text-2xl px-6 pt-3 text-claret">Data Tools</header>
         <div class="flex flex-wrap rounded-lg px-4 py-2">
-          <div class="my-3 mx-2 flex flex-wrap gap-y-3 gap-x-1">
-            <span class="font-lato bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Skicit Learn</span>
-            <span class="font-lato bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Pandas</span>
-            <span class="font-lato bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Numpy</span>
-            <span class="font-lato bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Microsoft Word</span>
-            <span class="font-lato bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Tensorflow</span>
-            <span class="font-lato bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Seaborn</span>
-            <span class="font-lato bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Power BI</span>
-            <span class="font-lato bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Microsoft Excel</span>
-            <span class="font-lato bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Microsoft PowerPoint</span>
+          <div class="my-3 mx-2 flex flex-wrap gap-y-3 gap-x-1 text-sm md:text-lg font-lato">
+            <span class="bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Skicit Learn</span>
+            <span class="bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Pandas</span>
+            <span class="bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Numpy</span>
+            <span class="bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Microsoft Word</span>
+            <span class="bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Tensorflow</span>
+            <span class="bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Seaborn</span>
+            <span class="bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Power BI</span>
+            <span class="bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Microsoft Excel</span>
+            <span class="bg-dark-purple-2 hover:bg-tryan-purple text-smoke px-3 py-1 mr-3 rounded-full">Microsoft PowerPoint</span>
           </div>
         </div>
       </div>
-      <!-- End Skill -->
+      <!-- End Data Tools-->
     </div>
   </main>
 </template>
